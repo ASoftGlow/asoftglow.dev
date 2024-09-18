3006683733f19df3fb6001cc5cf9024838981ab1
ASoftGlow <me@asoftglow.dev>
2024-09-18 17:47:38 -0400

    Wrote US and ZvH drafts, improved markup reuse

--- .eleventy.js
+++ .eleventy.js
@@ -7,4 +7,21 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.setLiquidOptions({
         jsTruthy: true
     });
+    eleventyConfig.addShortcode("year", () => new Date().getFullYear().toString());
+    eleventyConfig.addPairedShortcode("msg",
+        /**@type {(c: string, a: string, d: string) => string} */(content, author, date) => /*html*/`
+        <li role="article" aria-roledescription="Message">
+            <strong>${author}</strong> <time datetime="${date}"><code>${new Date(date).toLocaleString("en-GB")}</code></time>
+            <p>${content}</p>
+        </li>`
+    );
+    eleventyConfig.addFilter("getLinkText",
+        /**@type {(link: string) => string} */(link) => {
+            switch (new URL(link, "https://asoftglow.dev").hostname) {
+                case "github.com": return "GitHub Repo";
+                case "asoftglow.dev":
+                case "localhost": return "Product";
+                default: return "Link";
+            }
+        });
 };

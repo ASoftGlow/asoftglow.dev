072a31dfb411c399dab3c53ef1bdb9f9c4f5ceb8
ASoftGlow <me@asoftglow.dev>
2024-10-10 03:02:07 -0400

    Added useless shell to index, added to and made ZvH more DRY

--- .eleventy.js
+++ .eleventy.js
@@ -8,6 +8,20 @@ module.exports = function (eleventyConfig) {
         jsTruthy: true
     });
     eleventyConfig.addShortcode("year", () => new Date().getFullYear().toString());
+    eleventyConfig.addPairedShortcode("convo",
+        /**@type {(c: string, a: string, d: string) => string} */(content, guild_link, msg_link) => /*html*/`
+        <div style="display: grid;">
+            <figure class="elevation">
+                <ul>
+                    ${content}
+                </ul>
+                <figcaption style="padding: 0 1em 1em;">
+                    <a href="${guild_link}">Guild invite</a> •
+                    <a href="${msg_link}">Msg link</a>
+                </figcaption>
+            </figure>
+        </div>`
+    );
     eleventyConfig.addPairedShortcode("msg",
         /**@type {(c: string, a: string, d: string) => string} */(content, author, date) => /*html*/`
         <li role="article" aria-roledescription="Message">
